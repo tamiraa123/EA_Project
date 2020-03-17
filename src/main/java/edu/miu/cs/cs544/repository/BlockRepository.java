c24ad7e007624f91884a585d30dbfeec8c940855
Solmn <solomonmolla10@gmail.com>
2020-03-17 14:47:45 -0500

    Added some methods to block repository

--- src/main/java/edu/miu/cs/cs544/repository/BlockRepository.java
+++ src/main/java/edu/miu/cs/cs544/repository/BlockRepository.java
@@ -3,5 +3,10 @@ package edu.miu.cs.cs544.repository;
 import edu.miu.cs.cs544.domain.Block;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BlockRepository extends JpaRepository<Block, Long> {
+    public List<Block> getBlocksByCode(String code);
+    public List<Block> getBlocksByName(String name);
+    public List<Block> getBlocksBySemester(String semester);
 }
