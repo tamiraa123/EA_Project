af88d7d01e6a6a3ffe8fc991321a5c224a9bc453
Solmn <solomonmolla10@gmail.com>
2020-03-17 15:05:05 -0500

    Edited block controller

--- src/main/java/edu/miu/cs/cs544/controller/BlockController.java
+++ src/main/java/edu/miu/cs/cs544/controller/BlockController.java
@@ -3,10 +3,7 @@ package edu.miu.cs.cs544.controller;
 import edu.miu.cs.cs544.domain.Block;
 import edu.miu.cs.cs544.service.BlockService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -15,27 +12,27 @@ public class BlockController {
     @Autowired
     private BlockService blockService;
 
-    @RequestMapping(value = "/blocks/{id}", method = RequestMethod.POST)
-    public void createBlock(@Valid @RequestBody Block block) {
-        blockService.addBlock(block);
-    }
-
-    @RequestMapping(value = "/blocks/{id}", method = RequestMethod.POST)
-    public Block editBlock(@Valid @RequestBody Block block, @PathVariable Long id) {
-        return blockService.editBlock(block, id);
-    }
-
-    @RequestMapping(value = "/blocks/{id}", method = RequestMethod.GET)
+    @GetMapping("/blocks/{id}")
     public Block getBlock(@PathVariable Long id) {
         return blockService.getBlock(id);
     }
 
-    @RequestMapping(value = "/blocks/", method = RequestMethod.GET)
+    @GetMapping("/blocks")
     public List<Block> getBlocks(){
         return blockService.getAllBlocks();
     }
 
-    @RequestMapping(value = "/blocks/{id}", method = RequestMethod.POST)
+    @PostMapping("/blocks")
+    public void createBlock(@Valid @RequestBody Block block) {
+        blockService.addBlock(block);
+    }
+
+    @PutMapping("/blocks/{id}")
+    public Block editBlock(@Valid @RequestBody Block block, @PathVariable Long blockId) {
+        return blockService.editBlock(block, blockId);
+    }
+
+    @DeleteMapping("/blocks/{id}")
     public void deleteBlock(@PathVariable Long id) {
         blockService.deleteBlock(id);
     }
